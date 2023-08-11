69b447ab73823fc086a26692be52fc6d06416ccb
zamarajade <125944832+zamarajade@users.noreply.github.com>
2023-08-11 01:18:59 +0000

    completed yes_no checker
    made yes_no function and completed it

---
+++ yes_no.py
@@ -0,0 +1,17 @@
+# function goes here
+def yes_no(question):
+
+  while True:
+    response = input(question).lower()
+    if response == "yes" or response == "y":
+      return "yes"
+    elif response == "no" or response == "n":
+      return "no"
+    else:
+      print("Please enter either yes or no.")
+
+# main routine goes here
+want_instructions = yes_no("Do you need instructions for this program? Y/N ").lower()
+
+if want_instructions == "yes":
+  print("Instructions go here")
